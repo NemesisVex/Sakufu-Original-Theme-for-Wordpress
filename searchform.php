276955472ecf4ff16142d838abb94544a5254cb7
Greg Bueno <greg@gregbueno.com>
2014-10-15 20:55:36 -0700

    Converting to a child theme of VexVox.

--- searchform.php
+++
@@ -1,7 +0,0 @@
-	<form role="search" method="get" class="search-form form-inline" action="<?php echo home_url( '/' ); ?>">
-		<div class="form-group">
-			<span class="screen-reader-text"><label>Search for:</label></span>
-		<input type="search" class="search-field form-control" placeholder="" value="" name="s" title="Search for:" />
-		<input type="submit" class="search-submit btn btn-default" value="Search" />
-		</div>
-</form>
